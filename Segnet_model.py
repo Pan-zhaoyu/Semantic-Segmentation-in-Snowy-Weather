80f9f579c9d45d2f8e1313d3cdd1283aace94f89
Pan-zhaoyu <eyezhugeice@gmail.com>
2018-07-18 17:35:43 +0900

    Add files via upload

--- Segnet_model.py
+++ Segnet_model.py
@@ -1,95 +1,103 @@
-#coding=utf-8
-import matplotlib
-matplotlib.use("Agg")
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, Reshape, Permute, Activation, Flatten, Dense
+from __future__ import absolute_import
+from __future__ import print_function
+import os
 
-#img_w = 736
-#img_h = 1280
-nb_class = 18
 
-def SegNet(img_w = 352, img_h= 480):
-    model = Sequential()
-    #encoder  
-    model.add(Conv2D(64,(3,3),strides=(1,1),input_shape=(img_w, img_h, 3),padding='same',activation='relu'))
-    model.add(BatchNormalization())  
-    model.add(Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(MaxPooling2D(pool_size=(2,2)))  
-    #(128,128)  
-    model.add(Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(MaxPooling2D(pool_size=(2, 2)))  
-    #(64,64)  
-    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(MaxPooling2D(pool_size=(2, 2)))  
-    #(32,32)  
-    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(MaxPooling2D(pool_size=(2, 2)))  
-    #(16,16)  
-#    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))
-#    model.add(BatchNormalization())
-#    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))
-#    model.add(BatchNormalization())
-#    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))
-#    model.add(BatchNormalization())
-#    model.add(MaxPooling2D(pool_size=(2, 2)))
-#    #(8,8)
-#    #decoder
-    model.add(UpSampling2D(size=(2,2)))
-#    #(16,16)
-#    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))
-#    model.add(BatchNormalization())
-#    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))
-#    model.add(BatchNormalization())
-#    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))
-#    model.add(BatchNormalization())
-#    model.add(UpSampling2D(size=(2, 2)))
-    #(32,32)  
-    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(UpSampling2D(size=(2, 2)))  
-    #(64,64)  
-    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(UpSampling2D(size=(2, 2)))  
-    #(128,128)  
-    model.add(Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(UpSampling2D(size=(2, 2)))  
-    #(256,256)  
-    model.add(Conv2D(64, (3, 3), strides=(1, 1), input_shape=(img_w, img_h, 3), padding='same', activation='relu'))
-    model.add(BatchNormalization())  
-    model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation='relu'))  
-    model.add(BatchNormalization())  
-    model.add(Conv2D(nb_class, (1, 1), strides=(1, 1), padding='same'))
-#    model.add(Reshape((nb_class,img_w, img_h)))
-    #axis=1和axis=2互换位置，等同于np.swapaxes(layer,1,2)
-#    model.add(Permute((2,1)))
-#    model.add(Reshape((img_w, img_h, nb_class)))
-    model.add(Activation('softmax'))
-#    model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])
-    model.summary()
-    return model
+import keras.models as models
+from keras.layers.core import Layer, Dense, Dropout, Activation, Flatten, Reshape, Permute
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+
+
+from keras import backend as K
+
+import cv2
+import numpy as np
+np.random.seed(07) # 0bserver07 for reproducibility
+
+data_shape = 352*480
+
+
+
+def create_encoding_layers():
+    kernel = 3
+    filter_size = 64
+    pad = 1
+    pool_size = 2
+    return [
+        ZeroPadding2D(padding=(pad,pad)),
+        Convolution2D(filter_size, (kernel, kernel), border_mode='valid'),
+        BatchNormalization(),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(pool_size, pool_size)),
+
+        ZeroPadding2D(padding=(pad,pad)),
+        Convolution2D(128, (kernel, kernel), border_mode='valid'),
+        BatchNormalization(),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(pool_size, pool_size)),
+
+        ZeroPadding2D(padding=(pad,pad)),
+        Convolution2D(256, (kernel, kernel), border_mode='valid'),
+        BatchNormalization(),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(pool_size, pool_size)),
+
+        ZeroPadding2D(padding=(pad,pad)),
+        Convolution2D(512, (kernel, kernel), border_mode='valid'),
+        BatchNormalization(),
+        Activation('relu'),
+    ]
+
+def create_decoding_layers():
+    kernel = 3
+    filter_size = 64
+    pad = 1
+    pool_size = 2
+    return[
+        ZeroPadding2D(padding=(pad,pad)),
+        Convolution2D(512, (kernel, kernel), border_mode='valid'),
+        BatchNormalization(),
+
+        UpSampling2D(size=(pool_size,pool_size)),
+        ZeroPadding2D(padding=(pad,pad)),
+        Convolution2D(256, (kernel, kernel), border_mode='valid'),
+        BatchNormalization(),
+
+        UpSampling2D(size=(pool_size,pool_size)),
+        ZeroPadding2D(padding=(pad,pad)),
+        Convolution2D(128, (kernel, kernel), border_mode='valid'),
+        BatchNormalization(),
+
+        UpSampling2D(size=(pool_size,pool_size)),
+        ZeroPadding2D(padding=(pad,pad)),
+        Convolution2D(filter_size, (kernel, kernel), border_mode='valid'),
+        BatchNormalization(),
+    ]
+
+
+
+
+segnet_basic = models.Sequential()
+
+segnet_basic.add(Layer(input_shape=(352, 480, 3)))
+
+
+
+segnet_basic.encoding_layers = create_encoding_layers()
+for l in segnet_basic.encoding_layers:
+    segnet_basic.add(l)
+
+# Note: it this looks weird, that is because of adding Each Layer using that for loop
+# instead of re-writting mode.add(somelayer+params) everytime.
+
+segnet_basic.decoding_layers = create_decoding_layers()
+for l in segnet_basic.decoding_layers:
+    segnet_basic.add(l)
+
+segnet_basic.add(Convolution2D(20, 1, 1, border_mode='valid',))
+
+segnet_basic.add(Reshape((20,data_shape), input_shape=(20,352,480)))
+segnet_basic.add(Permute((2, 1)))
+segnet_basic.add(Activation('softmax'))
+segnet_basic.compile(loss="categorical_crossentropy", optimizer='adadelta', metrics=["accuracy"])
