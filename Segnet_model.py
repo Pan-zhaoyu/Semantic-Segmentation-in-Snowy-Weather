66b778cf645912d3e921a35b980200d9eeb394d7
Pan-zhaoyu <eyezhugeice@gmail.com>
2018-07-26 16:50:30 +0900

    Update Segnet_model.py

--- Segnet_model.py
+++ Segnet_model.py
@@ -1,3 +1,4 @@
+#https://github.com/0bserver07/Keras-SegNet-Basic/blob/master/Segnet-Evaluation-Visualization.ipynbz
 from __future__ import absolute_import
 from __future__ import print_function
 import os
